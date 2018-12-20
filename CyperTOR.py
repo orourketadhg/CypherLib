d9ddecce3523089640ae366f2a2cec03df4f34f7
Tadhg O'Rourke <orourketadhg@gmail.com>
2018-12-20 19:25:17 +0000

    Initial commit of cypher library project

---
+++ CyperTOR.py
@@ -0,0 +1,30 @@
+"""
+Author: Tadhg O'Rourke
+Date: 20/12/2018
+
+Project Description:
+"""
+
+
+def ceaser_cypher_encrypt(plaintext, offset):
+    pass
+
+
+def ceaser_cypher_decrypt(cyphertext, offset):
+    pass
+
+
+def hill_cypher_encrypt():
+    pass
+
+
+def hill_cypher_decrypt():
+    pass
+
+
+def vigenere_cypher_encrypt():
+    pass
+
+
+def vigenere_cypher_decrypt():
+    pass
