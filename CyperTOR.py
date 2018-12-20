85a45e3359c4a7621fb0678868314f267062e02d
Tadhg O'Rourke <orourketadhg@gmail.com>
2018-12-20 21:20:17 +0000

    Ceaser Cypher Encryption - simple message encryption working

--- CyperTOR.py
+++ CyperTOR.py
@@ -6,25 +6,51 @@ Project Description:
 """
 
 
+def _cypher_alphabet():
+    return 'abcdefghijklmnopqrstuvwxyz'
+
+
 def ceaser_cypher_encrypt(plaintext, offset):
-    pass
+    alphabet_split = list(_cypher_alphabet())
+    alphabet = _cypher_alphabet()
+    plaintext_split = list(plaintext.lower())
+    unknown_letter = []
+    encrypted_message = []
+
+    # loop through letters in plaintext
+    for letter in plaintext_split:
+        print(letter)
+
+        # if letter is not in known alphabet
+        if letter not in alphabet:
+            # add letter to unknown letters
+            unknown_letter.append(letter)
+            continue
+
+        # if letter is known
+        elif letter in alphabet:
+            letter_value = alphabet.index(letter)
+            encrypted_value = (letter_value + offset) % 26
+            encrypted_message.append(alphabet_split[encrypted_value])
+
+    print(''.join(encrypted_message))
 
 
 def ceaser_cypher_decrypt(cyphertext, offset):
     pass
 
 
-def hill_cypher_encrypt():
+def hill_cypher_2by2_encrypt(plaintext, a, b, c, d):
     pass
 
 
-def hill_cypher_decrypt():
+def hill_cypher_2by2_decrypt(cyphertext, a, b, c, d):
     pass
 
 
-def vigenere_cypher_encrypt():
+def vigenere_cypher_encrypt(plaintext, key):
     pass
 
 
-def vigenere_cypher_decrypt():
+def vigenere_cypher_decrypt(cyphertext, key):
     pass
